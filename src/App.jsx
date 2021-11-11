120b70d6f9e1340f39b2e340113db2d1522a2cce
Em aN <44680040+eman275@users.noreply.github.com>
2021-11-11 23:31:47 +0200

    add user page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Topbar from './components/topbar/Topbar'
 import './app.css'
 import Home from './pages/home/Home'
 import UserList from './pages/userList/UserList'
+import User from './pages/user/User'
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 const App = () => {
@@ -18,6 +19,9 @@ const App = () => {
                </Route>
                    <Route path="/users">
                <UserList/>
+               </Route>
+               <Route path="/user/:userId">
+               <User/>
                </Route>
              </Switch>
             
---
+++ src/pages/user/User.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import './user.css'
+import Users from '../../images/Eman.jpeg'
+
+import {PermIdentity , CalendarToday ,PhoneAndroid , MailOutline ,LocationSearching,Publish} from '@material-ui/icons'
+import { Link } from 'react-router-dom'
+
+const User = () => {
+    return (
+        <div className="user">
+            <div className="userTitleContainer">
+                <h1 className="userTitle">Edit User</h1>
+                <Link to="/newUser">
+                <button className="userAddButton">Create</button>
+                 </Link>
+                
+            </div>
+             <div className="userContainer">
+                 <div className="userShow">
+                     <div className="userShowTop">
+                         <img 
+                         className="userShowImg"
+                         src={Users} 
+                         alt="" />
+                         <div className="userShowTopTitle">
+                         <span className="userShowUsername">Eman M.Mostafa</span>
+                         <span className="userShowUserTitle">  ForntEnd Developer</span>
+                         </div>
+                     </div>
+                     <div className="userShowBottom">
+                         <span className="userShowTitle">Account Details</span>
+                         <div className="userShowInfo">
+                         <PermIdentity className="userShowIcon"/>
+                         <span className="userShowInfoTitle"> Emaaw27</span>
+                         </div>
+                         <div className="userShowInfo">
+                    <CalendarToday className="userShowIcon" />
+                  <span className="userShowInfoTitle">10.12.1999</span>
+                  </div>
+               <span className="userShowTitle">Contact Details</span>
+            <div className="userShowInfo">
+              <PhoneAndroid className="userShowIcon" />
+              <span className="userShowInfoTitle">+20 11 556 65 11</span>
+            </div>
+            <div className="userShowInfo">
+              <MailOutline className="userShowIcon" />
+              <span className="userShowInfoTitle">emanfk96@gmail.com</span>
+            </div>
+            <div className="userShowInfo">
+              <LocationSearching className="userShowIcon" />
+              <span className="userShowInfoTitle">Cairo| Egypt</span>
+            </div>
+                     </div>
+
+                 </div>
+                 <div className="userUpdate">
+                 <span className="userUpdateTitle">Edit</span>
+                 <form action="" className="userUpdateForm">
+                       <div className="userUpdateLef">
+                         <div className="userUpdateItem">
+                               <label >Username</label>
+                               <input  type="text"  placeholder="Emaaw27" className="userUpdateInput" />
+                             </div> 
+                             <div className="userUpdateItem">
+                <label>Full Name</label>
+                <input
+                  type="text"
+                  placeholder="Eman M.Mostafa"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Email</label>
+                <input
+                  type="text"
+                  placeholder="emaan529@gmail.com"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Phone</label>
+                <input
+                  type="text"
+                  placeholder="+20 12 456 67"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Address</label>
+                <input
+                  type="text"
+                  placeholder="Cairo | Egypt"
+                  className="userUpdateInput"
+                />
+              </div>                          
+
+                       </div>
+                       <div className="userUpdateRight">
+                           <div className="userUpdateUpload">
+                                <img src={Users} alt="" className="userUpdateImg" />
+                                  <label htmlFor="file"><Publish className="userUpdateIcon"/></label>
+                                    <input type="file" id="file" style={{display:"none"}} />
+                           </div>
+                           <button className="userUpdateButton">Update</button>
+                       </div>
+                 </form>
+                 </div>
+                 
+
+             </div>
+        </div>
+    )
+}
+
+export default User
